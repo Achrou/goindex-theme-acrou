12bfcc9746c84ec4502bd00f83c9dddeb2f57a84
fzfile <fzfileserver@gmail.com>
2020-06-22 14:25:02 +0200

    build 2.0.7

--- go2index/index.js
+++ go2index/index.js
@@ -67,7 +67,7 @@ var authConfig = {
 var themeOptions = {
   cdn: "https://cdn.jsdelivr.net/gh/fzfile/goindex-theme-acrou",
   // 主题版本号
-  version: "2.0.5",
+  version: "2.0.7",
   //可选默认系统语言:en/zh-chs/zh-cht
   languages: "en",
   render: {
